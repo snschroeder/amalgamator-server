4a22116fa1693af1c5e3cd58dfc195232e512fba
snschroeder <snschroeder@protonmail.com>
2020-04-27 17:19:10 -0600

    adds method to assemble parts of recipe into one object

--- recipe_scraper.py
+++ recipe_scraper.py
@@ -4,16 +4,30 @@ from pprint import pprint
 
 class RecipeScraper:
 
-    def fetch_raw_data(self, link):
+    def __fetch_raw_data(self, url):
         session = requests_html.HTMLSession()
-        r = session.get(link)
+        r = session.get(url)
         return r
 
-    def find_class(self, raw_html, class_name):
-        raw_html.html.render()
+    def __find_class(self, raw_html, class_name):
+        # raw_html.html.render()
         data = raw_html.html.find(class_name, first=True)
         pprint(data.text)
         return data
+
+    def assemble_recipe(self, url, class_arr):
+        recipe_dict = {}
+        headers = ['summary', 'ingredients', 'instructions']
+
+        raw_html = self.__fetch_raw_data(url)
+        raw_html.html.render()
+
+        for ind, val in enumerate(class_arr):
+            recipe_dict[headers[ind]] = self.__find_class(raw_html, val)
+
+        pprint(recipe_dict)
+        return recipe_dict
+
 
 r_scraper = RecipeScraper()
 
@@ -22,7 +36,6 @@ test_class_name = '.recipe-info-section' # correctly returns recipe summary
 test2_class_name = '.ingredients-section' # correctly returns ingredients
 test3_class_name = '.instructions-section' # correctly returns recipe instructions
 
+test_arr = ['.recipe-info-section', '.ingredients-section', '.instructions-section']
 
-raw_html = r_scraper.fetch_raw_data(test_url)
-
-r_scraper.find_class(raw_html, test2_class_name)
+r_scraper.assemble_recipe(test_url, test_arr)
